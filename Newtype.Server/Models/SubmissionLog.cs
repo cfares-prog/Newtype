e02a3136407d73b47c8a9fce9ea88134f1295c16
cfares-prog <cfares405@gmail.com>
2026-05-17 23:04:09 +0300

    DB setup 1

---
+++ Newtype.Server/Models/SubmissionLog.cs
@@ -0,0 +1,11 @@
+namespace Newtype.Server.Models;
+
+public class Submissionlog
+{
+    public int Id { get; set; }
+    public string ConnectionId { get; set; } = string.Empty;
+    public string FileName { get; set; } = string.Empty;
+    public string SourceCode { get; set; } = string.Empty;
+    public bool IsSuccess { get; set; }
+    public DateTime CompilationData { get; set; }
+}
